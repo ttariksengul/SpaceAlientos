c9db004c45c340cb046e93b29f7ec2b863596e1a
ttariksengul <tariksengul15180@gmail.com>
2024-06-16 14:55:26 +0300

    SpaceAlientos
    space game

---
+++ Assets/Scripts/ProjectileSpawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileSpawner : MonoBehaviour
+{
+    public GameObject enemyProjectile;
+    public float spawnTimer;
+    public float spawnMax = 10;
+    public float spawnMin = 5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnTimer = Random.Range(spawnMin, spawnMax);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimer -= Time.deltaTime;
+        if(spawnTimer <= 0)
+        {
+            Instantiate(enemyProjectile, transform.position, Quaternion.identity);
+            spawnTimer = Random.Range(spawnMin, spawnMax);
+
+        }
+    }
+}
